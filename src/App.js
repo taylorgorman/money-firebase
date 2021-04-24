40aab01e5dca671f4d27786527a8aa2a702b3737
Taylor Gorman <me@taylorpatrickgorman.com>
2021-04-24 17:37:31 -0500

    firebase setup, basic auth complete!
    holy shit, react-firebase-hooks and https://www.youtube.com/watch?v=zQyrwxMPm88 are AMAZING. this is worlds better/faster than the last time i tried any of this stuff.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import './App.css';
+import './App.css'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-function App() {
+if (firebase.apps.length === 0) { // for react dev live refresh
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  });
+}
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
+export default function App() {
+  const [user, loading, error] = useAuthState(auth);
+  console.log({ user, loading, error });
+
   return (
     <>
       <header>
         <h1>Money</h1>
-        <div>
-          <button>Log in</button>
-        </div>
+        <div>{user?.displayName}</div>
+        <div>{user ? <SignOutButton /> : <SignInButton />}</div>
       </header>
-      <main>
-        <p>You are not logged in.</p>
-      </main>
+      <main>{user ? <p>Welcome!</p> : <p>You are not logged in.</p>}</main>
     </>
   );
 }
 
-export default App;
+function SignInButton() {
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOutButton() {
+  return (
+    auth.currentUser && (
+      <button onClick={ () => auth.signOut() }>Sign out</button>
+    )
+  );
+}
