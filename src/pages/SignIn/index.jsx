6ff173e141d459330f4dd221291fafb6ecefaab6
Taylor Gorman <me@taylorpatrickgorman.com>
2021-06-03 01:22:12 -0500

    sign in uses card

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,17 +3,20 @@ import Section from '../../components/Section'
 import { SignInWithGoogleButton, SignInWithFacebookButton, SignInWithTwitterButton } from '../../components/AuthButtons'
 
 import './styles.scss'
+import { Card } from 'react-bootstrap'
 
 export default function SignIn() {
   return (
     <Layout>
       <Section className="sign-in">
-        <h1>Sign in</h1>
-        <p>
-          <SignInWithGoogleButton />
-          <SignInWithFacebookButton />
-          <SignInWithTwitterButton />
-        </p>
+        <Card body>
+          <h1>Sign in</h1>
+          <p>
+            <SignInWithGoogleButton />
+            <SignInWithFacebookButton />
+            <SignInWithTwitterButton />
+          </p>
+        </Card>
       </Section>
     </Layout>
   )
