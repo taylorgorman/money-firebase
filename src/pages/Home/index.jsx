78d1e47936464e890f006c95a6d1385f2f39eada
Taylor Gorman <me@taylorpatrickgorman.com>
2021-06-11 19:03:35 -0500

    competitors as card group instead of table
    this was v unnecessary, but it looks way cleaner. layout is much harder than wth a table, tho.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
 import Competitors from '../../components/Competitors'
 import Layout from '../../components/Layout'
+import PageHeading from '../../components/PageHeading'
+import Section from '../../components/Section'
 
 export default function Home() {
   return (
     <Layout>
-      <Competitors />
+      <Section>
+        <PageHeading>Sir Pigglesworth</PageHeading>
+        <p className="mt-0"><em>Organize your money like fancy swine.</em></p>
+        <h2>Competitors</h2>
+        <Competitors />
+      </Section>
     </Layout>
   )  
 }
