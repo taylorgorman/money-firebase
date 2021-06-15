b0c9748acd512b9d631c34973d4cbec38593097b
Taylor Gorman <me@taylorpatrickgorman.com>
2021-06-15 01:21:18 -0500

    accounts retrieves from firestore
    next need create, update, delete

--- src/pages/Accounts/AccountsRows.jsx
+++ src/pages/Accounts/AccountsRows.jsx
@@ -1,31 +1,48 @@
 import { Badge, Card } from 'react-bootstrap'
 
 import './AccountsRows.scss'
-import data from './data.json'
 import Currency from '../../components/ui/Currency'
 import AreaChart from '../../components/AreaChart'
+import { useData } from '../../utilities/DataContext'
 
 export default function AccountsRows( { showNetWorth } ) {
+  const { retrieveData } = useData()
+  const [accounts, loading, error] = retrieveData( 'accounts' )
+
   return (
-    <div className="accounts-rows">
-      { data.map( ( account ) => (
-        <Card body className="card-account account-row" key={ account.name }>
-          <span className="flex-item name-bank">
-            { account.name }
-            <span className="bank">{ account.bank }</span>
-          </span>
-          <span className="flex-item type">
-            <Badge variant="light">{ account.type }</Badge>
-          </span>
-          <AreaChart className="flex-item" />
-          <Currency className="flex-item" amount={ account.balance } />
-        </Card>
-      ) ) }
-      { showNetWorth && (
-        <div className="total">
-          <Currency amount={ data.reduce( ( total, account ) => total + account.balance, 0 ) } />
-        </div>
+    <>
+      { loading && (
+        <p>Loading accounts...</p>
       ) }
-    </div>
+      { error && (
+      <p>
+        Error loading accounts:
+        { ' ' }
+        <code>{ JSON.stringify( error ) }</code>
+      </p>
+      ) }
+      <div className="accounts-rows">
+        { accounts.map( ( account ) => (
+          <Card body className="card-account account-row" key={ account.name }>
+            <span className="flex-item name-bank">
+              { account.name }
+              <span className="bank">{ account.bank }</span>
+            </span>
+            <span className="flex-item type">
+              <Badge variant="light">{ account.type }</Badge>
+            </span>
+            <AreaChart className="flex-item" />
+            <Currency className="flex-item" amount={ account.balance } />
+          </Card>
+        ) ) }
+        { showNetWorth && (
+        <div className="total">
+          <Currency
+            amount={ accounts.reduce( ( total, account ) => total + account.balance, 0 ) }
+          />
+        </div>
+        ) }
+      </div>
+    </>
   )
 }
